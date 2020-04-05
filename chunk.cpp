192a13758c8ab5b01487db205a26bfa5b0914bcd
marcus-elia <mselia@uvm.edu>
2020-04-05 16:45:05 -0400

    Copied in code from random-3d-city

---
+++ chunk.cpp
@@ -0,0 +1,123 @@
+#include "chunk.h"
+
+Chunk::Chunk()
+{
+    bottomLeft = {0,0};
+    sideLength = 1024;
+    perlinSeed = 0.5;
+    initializeCenter();
+    propertySize = sideLength / 8;
+}
+Chunk::Chunk(Point2D inputBottomLeft, int inputSideLength,  double inputPerlinSeed)
+{
+    bottomLeft = inputBottomLeft;
+    sideLength = inputSideLength;
+    perlinSeed = inputPerlinSeed;
+    initializeCenter();
+    propertySize = sideLength / 8;
+}
+
+void Chunk::initializeCenter()
+{
+    center = {sideLength*bottomLeft.x + sideLength/2, sideLength*bottomLeft.z - sideLength/2};
+}
+
+
+// Getters
+Point2D Chunk::getBottomLeft() const
+{
+    return bottomLeft;
+}
+int Chunk::getSideLength() const
+{
+    return sideLength;
+}
+Point2D Chunk::getCenter() const
+{
+    return center;
+}
+
+void Chunk::draw() const
+{
+    glBegin(GL_QUADS);
+    /*if((bottomLeft.x + bottomLeft.z) % 2 == 0)
+    {
+        glColor4f(1, 1, 0.3, 1);
+    }
+    else
+    {
+        glColor4f(0, 1, 0.8, 1);
+    }*/
+    //glColor4f(perlinSeed, 0, 1, 1);
+    glColor4f(0.5, 0.5, 0.5, 1);
+    glVertex3f(sideLength*bottomLeft.x,0, sideLength*bottomLeft.z);
+    glVertex3f(sideLength*bottomLeft.x,0, sideLength*bottomLeft.z + sideLength);
+    glVertex3f(sideLength*bottomLeft.x + sideLength,0, sideLength*bottomLeft.z + sideLength);
+    glVertex3f(sideLength*bottomLeft.x + sideLength,0, sideLength*bottomLeft.z);
+
+    glEnd();
+}
+
+int Chunk::chunkToInt() const
+{
+    return pointToInt({bottomLeft.x, bottomLeft.z});
+}
+
+std::vector<Point2D> Chunk::getChunksAround(int radius)
+{
+    return getChunksAroundPointByPoint(bottomLeft, radius);
+}
+
+int pointToInt(Point2D p)
+{
+    int a = p.x;
+    int b = p.z;
+    if(a > 0 && b <= a-1 && b > -a)
+    {
+        return 4*a*a - 3*a - b;
+    }
+    else if(b < 0 && a <= -b && a > b+1)
+    {
+        return 4*b*b + b - a;
+    }
+    else if(a < 0 && b > a-1 && b < -a)
+    {
+        return 4*a*a - a + b;
+    }
+    else if(b > -1 && a >= -b && a < b+1)
+    {
+        return 4*b*b + 3*b + a;
+    }
+    return (2*a-1)*(2*a-3); // idk why, but this works for (0,-1) and (-1,-2)
+}
+
+std::vector<int> getChunksAroundPoint(Point2D p, int radius)
+{
+    std::vector<int> result;
+
+    // Start at the top of the diamond and work down from there
+    for(int b = p.z + radius; b >= p.z - radius; b--)
+    {
+        int distanceFromZ = abs(b - p.z);
+        for(int a = p.x - (radius - distanceFromZ); a <= p.x + (radius - distanceFromZ); a++)
+        {
+            result.push_back(pointToInt({a,b}));
+        }
+    }
+    return result;
+}
+std::vector<Point2D> getChunksAroundPointByPoint(Point2D p, int radius)
+{
+    std::vector<Point2D> result;
+
+    // Start at the top of the diamond and work down from there
+    for(int b = p.z + radius; b >= p.z - radius; b--)
+    {
+        int distanceFromZ = abs(b - p.z);
+        for(int a = p.x - (radius - distanceFromZ); a <= p.x + (radius - distanceFromZ); a++)
+        {
+            result.push_back({a,b});
+        }
+    }
+    return result;
+}
---
+++ chunk.h
@@ -0,0 +1,74 @@
+#ifndef RANDOM_3D_CITY_CHUNK_H
+#define RANDOM_3D_CITY_CHUNK_H
+
+
+
+
+#include "graphics.h"
+//#include "building.h"
+#include <vector>
+#include <stdlib.h>
+#include <time.h>
+
+// int points used for the chunks
+// in the xz plane
+struct Point2D
+{
+    int x;
+    int z;
+    bool operator ==(const Point2D& p1)
+    {
+        return x == p1.x && z == p1.z;
+    }
+    bool operator !=(const Point2D& p1)
+    {
+        return x != p1.x || z != p1.z;
+    }
+};
+
+
+class Chunk
+{
+private:
+    Point2D bottomLeft; // The bottom left coordinate divided by sideLength
+    int sideLength;
+    Point2D center;   // The actual center
+
+    // The number of the chunk based on its location
+    int chunkID;
+
+    // The value of the Perlin noise map for this chunk
+    double perlinSeed;
+
+    int propertySize; // = side length of square assigned to each building
+public:
+    Chunk();
+    Chunk(Point2D inputBottomLeft, int inputSideLength, double inputPerlinSeed);
+
+    void initializeCenter();
+
+    // Getters
+    Point2D getBottomLeft() const;
+    int getSideLength() const;
+    Point2D getCenter() const;
+
+    void draw() const;
+
+    // Wrapper function, returns chunks around this
+    std::vector<Point2D> getChunksAround(int radius);
+
+    // Divides by size and converts resulting ordered pair to int
+    int chunkToInt() const;
+};
+
+// Returns the result of mapping a 2d point in Z x Z into the
+// non-negative integers, by spiraling clockwise.
+int pointToInt(Point2D p);
+
+// Returns the ints corresponding to to all chunks that are within radius of this one,
+// using the taxicab metric
+std::vector<int> getChunksAroundPoint(Point2D p, int radius);
+std::vector<Point2D> getChunksAroundPointByPoint(Point2D p, int radius);
+
+
+#endif //RANDOM_3D_CITY_CHUNK_H
