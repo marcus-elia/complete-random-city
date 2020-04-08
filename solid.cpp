5cd7d2eadc114af2112a6817be7b652b66aab137
marcus-elia <mselia@uvm.edu>
2020-04-08 16:34:47 -0400

    Copied in Solid class

---
+++ solid.cpp
@@ -0,0 +1,96 @@
+#include "solid.h"
+
+Solid::Solid()
+{
+    xWidth = 1;
+    yWidth = 1;
+    zWidth = 1;
+    lineColor = {1,1,1,1};
+    initializeCorners();
+}
+
+Solid::Solid(Point inputCenter, RGBAcolor inputColor,
+             double inputXWidth, double inputYWidth, double inputZWidth, RGBAcolor inputLineColor)
+{
+    center = inputCenter;
+    color = inputColor;
+    xWidth = inputXWidth;
+    yWidth = inputYWidth;
+    zWidth = inputZWidth;
+    lineColor = inputLineColor;
+    initializeCorners();
+}
+
+void Solid::initializeCorners()
+{
+
+}
+
+Point Solid::getCenter() const
+{
+    return center;
+}
+RGBAcolor Solid::getColor() const
+{
+    return color;
+}
+std::vector<Point> Solid::getCorners() const
+{
+    return corners;
+}
+double Solid::getXWidth() const
+{
+    return xWidth;
+}
+double Solid::getYWidth() const
+{
+    return yWidth;
+}
+double Solid::getZWidth() const
+{
+    return zWidth;
+}
+RGBAcolor Solid::getLineColor() const
+{
+    return lineColor;
+}
+
+
+void Solid::setCenter(Point inputCenter)
+{
+    center = inputCenter;
+}
+void Solid::setColor(RGBAcolor inputColor)
+{
+    color = inputColor;
+}
+void Solid::setCorners(std::vector<Point> inputCorners)
+{
+    corners = inputCorners;
+}
+void Solid::setXWidth(double inputXWidth)
+{
+    xWidth = inputXWidth;
+}
+void Solid::setYWidth(double inputYWidth)
+{
+    yWidth = inputYWidth;
+}
+void Solid::setZWidth(double inputZWidth)
+{
+    zWidth = inputZWidth;
+}
+void Solid::setLineColor(RGBAcolor inputLineColor)
+{
+    lineColor = inputLineColor;
+}
+
+double distance2d(Point p1, Point p2)
+{
+    return sqrt((p1.x - p2.x)*(p1.x - p2.x) + (p1.z - p2.z)*(p1.z - p2.z));
+}
+
+double directedDistance(double A, double B, double C, double x, double y)
+{
+    return (A*x + B*y + C) / sqrt(A*A + B*B);
+}
---
+++ solid.h
@@ -0,0 +1,59 @@
+#ifndef COMPLETE_RANDOM_CITY_SOLID_H
+#define COMPLETE_RANDOM_CITY_SOLID_H
+
+#include "structs.h"
+#include <vector>
+#include <experimental/optional>
+#include <cmath>
+
+class Solid
+{
+protected:
+    Point center;
+    RGBAcolor color;
+    std::vector<Point> corners;
+    double xWidth;
+    double yWidth;
+    double zWidth;
+    RGBAcolor lineColor;
+public:
+    // Constructors
+    Solid();
+    Solid(Point inputCenter, RGBAcolor inputColor,
+          double inputXWidth, double inputYWidth, double inputZWidth, RGBAcolor inputLineColor);
+
+    // Initialization
+    virtual void initializeCorners();
+
+    // Getters
+    Point getCenter() const;
+    RGBAcolor getColor() const;
+    std::vector<Point> getCorners() const;
+    double getXWidth() const;
+    double getYWidth() const;
+    double getZWidth() const;
+    RGBAcolor getLineColor() const;
+
+    // Setter
+    void setCenter(Point inputCenter);
+    void setColor(RGBAcolor inputColor);
+    void setCorners(std::vector<Point> inputCorners);
+    void setXWidth(double inputXWidth);
+    void setYWidth(double inputYWidth);
+    void setZWidth(double inputZWidth);
+    void setLineColor(RGBAcolor inputLineColor);
+
+    // If the p is within buffer of this shape, a corrected position
+    // is returned. returns nullopt otherwise.
+    virtual std::experimental::optional<Point> correctCollision(Point p, int buffer)=0;
+
+    virtual void printDebugStats()=0;
+};
+
+// Returns the distance in the xz plane
+double distance2d(Point p1, Point p2);
+
+// 2d directed distance
+double directedDistance(double A, double B, double C, double x, double y);
+
+#endif //COMPLETE_RANDOM_CITY_SOLID_H
