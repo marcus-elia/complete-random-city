5dbfc857507f71e89e1a22c983ea0c2a6210434e
marcus-elia <mselia@uvm.edu>
2020-04-08 21:50:41 -0400

    Restored color to buildings

--- building.cpp
+++ building.cpp
@@ -33,92 +33,98 @@ void Building::initializeSolids()
     if(buildingType == Plain)
     {
         Point center = {(double)topLeft.x + sideLength/2, (double)height/2, (double)topLeft.z + sideLength/2};
-        solids.push_back(std::make_shared<RecPrism>(RecPrism(center, gray,
-                                                             sideLength, height, sideLength, gray)));
+        solids.push_back(std::make_shared<RecPrism>(RecPrism(center, color,
+                                                             sideLength, height, sideLength, edgeColor)));
     }
     else if(buildingType == House)
     {
         Point center = {(double)topLeft.x + sideLength/2, (double)height/2, (double)topLeft.z + sideLength/2};
-        solids.push_back(std::make_shared<RecPrism>(RecPrism(center, gray,
-                                                             sideLength - 5, height, sideLength - 5, gray)));
+        solids.push_back(std::make_shared<RecPrism>(RecPrism(center, color,
+                                                             sideLength - 5, height, sideLength - 5,
+                                                             edgeColor)));
         Point roofCenter = {center.x, center.y + 3*height/4, center.z};
-        solids.push_back(std::make_shared<TriPrism>(TriPrism(roofCenter, gray,
-                                                             sideLength, height/2, sideLength, gray)));
+        solids.push_back(std::make_shared<TriPrism>(TriPrism(roofCenter, color,
+                                                             sideLength, height/2, sideLength, edgeColor)));
     }
     else if(buildingType == Skyscraper)
     {
         Point center = {(double)topLeft.x + sideLength/2, (double)3*height/4, (double)topLeft.z + sideLength/2};
-        solids.push_back(std::make_shared<RecPrism>(RecPrism(center, gray,
-                                                             sideLength, 3*height/2, sideLength, gray)));
+        solids.push_back(std::make_shared<RecPrism>(RecPrism(center, color,
+                                                             sideLength, 3*height/2, sideLength, edgeColor)));
     }
     else if(buildingType == Empire)
     {
         Point center = {(double)topLeft.x + sideLength/2, (double)height/2, (double)topLeft.z + sideLength/2};
-        solids.push_back(std::make_shared<RecPrism>(RecPrism(center, gray,
-                                                             sideLength, height, sideLength, gray)));
+        solids.push_back(std::make_shared<RecPrism>(RecPrism(center, color,
+                                                             sideLength, height, sideLength, edgeColor)));
         center = {center.x, center.y + height/2.0 + height/12.0, center.z};
-        solids.push_back(std::make_shared<Frustum>(Frustum(center, gray,
-                                                           sideLength, height/6.0, sideLength, gray,
+        solids.push_back(std::make_shared<Frustum>(Frustum(center, color,
+                                                           sideLength, height/6.0, sideLength, edgeColor,
                                                            2*sideLength/3, 2*sideLength/3)));
         center = {center.x, center.y + height/12.0 + height/8.0, center.z};
-        solids.push_back(std::make_shared<RecPrism>(RecPrism(center, gray,
+        solids.push_back(std::make_shared<RecPrism>(RecPrism(center, color,
                                                              2*sideLength/3, height/4.0,
-                                                             2*sideLength/3, gray)));
+                                                             2*sideLength/3, edgeColor)));
         center = {center.x, center.y + height/8.0 + height/12.0, center.z};
-        solids.push_back(std::make_shared<Frustum>(Frustum(center, gray,
-                                                           2*sideLength/3, height/6.0, 2*sideLength/3, gray,
+        solids.push_back(std::make_shared<Frustum>(Frustum(center, color,
+                                                           2*sideLength/3, height/6.0,
+                                                           2*sideLength/3, edgeColor,
                                                            sideLength/3, sideLength/3)));
         center = {center.x, center.y + height/12.0 + height/4.0, center.z};
-        solids.push_back(std::make_shared<RecPrism>(RecPrism(center, gray,
-                                                             5, height/2.0, 5, gray)));
+        solids.push_back(std::make_shared<RecPrism>(RecPrism(center, color,
+                                                             5, height/2.0, 5, edgeColor)));
     }
     else if(buildingType == UFO)
     {
         Point center = {(double)topLeft.x + sideLength/2, (double)height/2, (double)topLeft.z + sideLength/2};
-        solids.push_back(std::make_shared<RecPrism>(RecPrism(center, gray,
-                                                             sideLength, height, sideLength, gray)));
+        solids.push_back(std::make_shared<RecPrism>(RecPrism(center, color,
+                                                             sideLength, height, sideLength, edgeColor)));
         center = {center.x, center.y + height/2.0 + height/48.0, center.z};
-        solids.push_back(std::make_shared<RecPrism>(RecPrism(center, gray,
-                                                             sideLength/2, height/24.0, sideLength/2, gray)));
+        solids.push_back(std::make_shared<RecPrism>(RecPrism(center, color,
+                                                             sideLength/2, height/24.0,
+                                                             sideLength/2, edgeColor)));
         center = {center.x, center.y + height/48.0 + height/24.0, center.z};
-        solids.push_back(std::make_shared<Cylinder>(Cylinder(center, gray,
-                                                             sideLength, height/12.0, sideLength, gray)));
+        solids.push_back(std::make_shared<Cylinder>(Cylinder(center, color,
+                                                             sideLength, height/12.0, sideLength, edgeColor)));
         center = {center.x, center.y + height/24.0 + height/48.0, center.z};
-        solids.push_back(std::make_shared<Frustum>(Frustum(center, gray,
-                                                           sideLength/3, height/24.0, sideLength/3, gray,
+        solids.push_back(std::make_shared<Frustum>(Frustum(center, color,
+                                                           sideLength/3, height/24.0,
+                                                           sideLength/3, edgeColor,
                                                            sideLength/4, sideLength/4)));
     }
     else if(buildingType == CN)
     {
         Point center = {(double)topLeft.x + sideLength/2, (double)3*height/4, (double)topLeft.z + sideLength/2};
-        solids.push_back(std::make_shared<Cylinder>(Cylinder(center, gray,
-                                                             sideLength/2, 3*height/2, sideLength/2, gray)));
+        solids.push_back(std::make_shared<Cylinder>(Cylinder(center, color,
+                                                             sideLength/2, 3*height/2,
+                                                             sideLength/2, edgeColor)));
         center = {center.x, center.y + 3*height/4 + height/24, center.z};
-        solids.push_back(std::make_shared<Cylinder>(Cylinder(center, gray,
-                                                             sideLength, height/12, sideLength, gray)));
+        solids.push_back(std::make_shared<Cylinder>(Cylinder(center, color,
+                                                             sideLength, height/12, sideLength, edgeColor)));
         center = {center.x, center.y + height/24 + height/2, center.z};
-        solids.push_back(std::make_shared<Cylinder>(Cylinder(center, gray,
-                                                             sideLength/4, height, sideLength/4, gray)));
+        solids.push_back(std::make_shared<Cylinder>(Cylinder(center, color,
+                                                             sideLength/4, height, sideLength/4, edgeColor)));
     }
     else if(buildingType == Hourglass)
     {
         Point center = {(double)topLeft.x + sideLength/2, (double)height/3, (double)topLeft.z + sideLength/2};
-        solids.push_back(std::make_shared<Frustum>(Frustum(center, gray,
-                                                           sideLength, 2*height/3, sideLength, gray,
+        solids.push_back(std::make_shared<Frustum>(Frustum(center, color,
+                                                           sideLength, 2*height/3, sideLength, edgeColor,
                                                            3*sideLength/4, 3*sideLength/4)));
         center = {center.x, center.y + 2*height/3, center.z};
-        solids.push_back(std::make_shared<Frustum>(Frustum(center, gray,
-                                                           3*sideLength/4, 2*height/3, 3*sideLength/4, gray,
+        solids.push_back(std::make_shared<Frustum>(Frustum(center, color,
+                                                           3*sideLength/4, 2*height/3,
+                                                           3*sideLength/4, edgeColor,
                                                            sideLength, sideLength)));
     }
     else if(buildingType == Pyramid)
     {
         Point center = {(double)topLeft.x + sideLength/2.0, (double)height/2, (double)topLeft.z + sideLength/2};
-        solids.push_back(std::make_shared<RecPrism>(RecPrism(center, gray,
-                                                             sideLength, height, sideLength, gray)));
+        solids.push_back(std::make_shared<RecPrism>(RecPrism(center, color,
+                                                             sideLength, height, sideLength, edgeColor)));
         center = {center.x, center.y + height/2.0 + height/20.0, center.z};
-        solids.push_back(std::make_shared<Frustum>(Frustum(center, gray,
-                                                           sideLength, height/10.0, sideLength, gray,
+        solids.push_back(std::make_shared<Frustum>(Frustum(center, color,
+                                                           sideLength, height/10.0, sideLength, edgeColor,
                                                            sideLength/20.0, sideLength/20.0)));
     }
 }
