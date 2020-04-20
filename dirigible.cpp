16520e4a07276ca73744dd9a9dc67cf960abe3b9
marcus-elia <mselia@uvm.edu>
2020-04-20 08:55:18 -0400

    Added basic Dirigible class

---
+++ dirigible.cpp
@@ -0,0 +1,93 @@
+#include "dirigible.h"
+
+Dirigible::Dirigible() : Vehicle()
+{
+    length = 20;
+    height = 10;
+    width = 10;
+    target = location;
+    turnSpeed = 0.1;
+}
+Dirigible::Dirigible(Point inputLocation, Point inputLookingAt, double inputSpeed, Point inputVelocity,
+double inputLength, double inputHeight, double inputWidth, RGBAcolor inputBodyColor,
+typeOfAirship inputAirshipType) : Vehicle(inputLocation, inputLookingAt, inputSpeed, inputVelocity)
+{
+    length = inputLength;
+    height = inputHeight;
+    width = inputWidth;
+    bodyColor = inputBodyColor;
+    airshipType = inputAirshipType;
+    initializeSolids();
+    target = location;
+    turnSpeed = 0.1;
+}
+
+void Dirigible::initializeSolids()
+{
+    if(airshipType == Blimp)
+    {
+        solids.push_back(std::make_shared<Ellipsoid>(Ellipsoid(location, bodyColor, width, height, length,
+                {1,1,1,1},
+                location, lookingAt, 0, {0,0,0}, location)));
+    }
+}
+
+// Getters
+double Dirigible::getLength() const
+{
+    return length;
+}
+double Dirigible::getWidth() const
+{
+    return width;
+}
+double Dirigible::getHeight() const
+{
+    return height;
+}
+
+// Setters
+void Dirigible::setLength(double inputLength)
+{
+    length = inputLength;
+}
+void Dirigible::setHeight(double inputHeight)
+{
+    height = inputHeight;
+}
+void Dirigible::setWidth(double inputWidth)
+{
+    width = inputWidth;
+}
+
+void Dirigible::draw() const
+{
+    for(std::shared_ptr<Solid> s : solids)
+    {
+        s->draw();
+    }
+}
+
+void Dirigible::tick()
+{
+    move(velocity.x, velocity.y, velocity.z);
+    if(distance2d(target, location) < 10)
+    {
+        setRandomTarget();
+    }
+    double angleToTurn = atan2(target.z - location.z, target.x - location.x) - xzAngle;
+    if(angleToTurn < 0)
+    {
+        rotate(0, turnSpeed, 0);
+    }
+    else if(angleToTurn > 0)
+    {
+        rotate(0, -turnSpeed, 0);
+    }
+}
+
+void Dirigible::setRandomTarget()
+{
+    double randAngle = (rand() % 100) * (2*PI / 100);
+    target = {location.x + cos(randAngle)*100, location.y, location.z + sin(randAngle)*100};
+}
---
+++ dirigible.h
@@ -0,0 +1,48 @@
+#ifndef COMPLETE_RANDOM_CITY_DIRIGIBLE_H
+#define COMPLETE_RANDOM_CITY_DIRIGIBLE_H
+
+#include "vehicle.h"
+#include "ellipsoid.h"
+#include "cylinder.h"
+#include "rightTrapezoid.h"
+
+enum typeOfAirship {Blimp, Balloon};
+
+class Dirigible : public Vehicle
+{
+private:
+    RGBAcolor bodyColor;
+    double length;
+    double height;
+    double width;
+    typeOfAirship airshipType;
+
+    // For movement
+    Point target;
+    double turnSpeed;
+public:
+    Dirigible();
+    Dirigible(Point inputLocation, Point inputLookingAt, double inputSpeed, Point inputVelocity,
+    double inputLength, double inputHeight, double inputWidth, RGBAcolor inputBodyColor,
+    typeOfAirship inputAirshipType);
+
+    void initializeSolids();
+
+    // Getters
+    double getLength() const;
+    double getWidth() const;
+    double getHeight() const;
+
+    // Setters
+    void setLength(double inputLength);
+    void setHeight(double inputHeight);
+    void setWidth(double inputWidth);
+
+    void draw() const;
+
+    void tick();
+
+    void setRandomTarget();
+};
+
+#endif //COMPLETE_RANDOM_CITY_DIRIGIBLE_H
