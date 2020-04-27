0d70f23b21a13266aa9f5b694c0058b453bd23e9
marcus-elia <mselia@uvm.edu>
2020-04-27 08:50:31 -0400

    Added Tree class

---
+++ tree.cpp
@@ -0,0 +1,48 @@
+#include "tree.h"
+
+Tree::Tree()
+{
+    location = {0,0,0};
+    trunkHeight = 30;
+    trunkRadius = 6;
+    leavesHeight = 25;
+    leavesXZRadius = 15;
+    trunkColor = {0.2, 0.1, 0.05, 1};
+    leavesColor = {0, 0.7, 0, 1};
+    initializeSolids();
+}
+Tree::Tree(Point inputLocation, int inputTrunkHeight, double inputTrunkRadius, double inputLeavesHeight, double inputLeavesXZRadius)
+{
+    location = inputLocation;
+    trunkHeight = inputTrunkHeight;
+    trunkRadius = inputTrunkRadius;
+    leavesHeight = inputLeavesHeight;
+    leavesXZRadius = inputLeavesXZRadius;
+    double r = (rand() % 100 ) / 100.0;
+    leavesColor = {r/4, 0.5 + r/2, r/4, 1};
+    trunkColor = {0.1 + r/8, 0.05 + r/12, r/12, 1};
+    initializeSolids();
+}
+
+void Tree::initializeSolids()
+{
+    Point center = {location.x, trunkHeight/2.0, location.z};
+    trunk = Cylinder(location, trunkColor, trunkRadius, trunkHeight, trunkRadius, {1,1,1,1});
+    center = {location.x, trunkHeight + leavesHeight/2.0, location.z};
+    leaves = Ellipsoid(location, leavesColor, leavesXZRadius, leavesHeight, leavesXZRadius, {1,1,1,1});
+}
+
+Point Tree::getLocation() const
+{
+    return location;
+}
+double Tree::getTrunkRadius() const
+{
+    return trunkRadius;
+}
+
+void Tree::draw() const
+{
+    trunk.draw();
+    leaves.draw();
+}
---
+++ tree.h
@@ -0,0 +1,31 @@
+#ifndef COMPLETE_RANDOM_CITY_TREE_H
+#define COMPLETE_RANDOM_CITY_TREE_H
+
+#include "cylinder.h"
+#include "ellipsoid.h"
+
+class Tree
+{
+private:
+    Point location;
+    Ellipsoid leaves;
+    Cylinder trunk;
+    int trunkHeight;
+    double trunkRadius;
+    double leavesHeight;
+    double leavesXZRadius;
+    RGBAcolor trunkColor;
+    RGBAcolor leavesColor;
+public:
+    Tree();
+    Tree(Point inputLocation, int inputTrunkHeight, double inputTrunkRadius, double inputLeavesHeight, double inputLeavesXZRadius);
+
+    void initializeSolids();
+
+    Point getLocation() const;
+    double getTrunkRadius() const;
+
+    void draw() const;
+};
+
+#endif //COMPLETE_RANDOM_CITY_TREE_H
