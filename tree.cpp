044b513e44d9434c62764f2950bb15b311537f2b
marcus-elia <mselia@uvm.edu>
2020-04-27 10:28:17 -0400

    Changed tree parameters to make the Ellipsoids more efficiently drawn

--- tree.cpp
+++ tree.cpp
@@ -20,16 +20,17 @@ Tree::Tree(Point inputLocation, int inputTrunkHeight, double inputTrunkRadius, d
     leavesXZRadius = inputLeavesXZRadius;
     double r = (rand() % 100 ) / 100.0;
     leavesColor = {r/4, 0.5 + r/2, r/4, 1};
-    trunkColor = {0.1 + r/8, 0.05 + r/12, r/12, 1};
+    trunkColor = {0.2 + r/8, 0.15 + r/12, r/12, 1};
     initializeSolids();
 }
 
 void Tree::initializeSolids()
 {
     Point center = {location.x, trunkHeight/2.0, location.z};
-    trunk = Cylinder(location, trunkColor, trunkRadius, trunkHeight, trunkRadius, {1,1,1,1});
+    trunk = Cylinder(center, trunkColor, trunkRadius, trunkHeight, trunkRadius, {1,1,1,1});
     center = {location.x, trunkHeight + leavesHeight/2.0, location.z};
-    leaves = Ellipsoid(location, leavesColor, leavesXZRadius, leavesHeight, leavesXZRadius, {1,1,1,1});
+    leaves = Ellipsoid(center, leavesColor, leavesXZRadius, leavesHeight, leavesXZRadius, {1,1,1,1},
+            true, 5, 6);
 }
 
 bool Tree::conflictsWithTree(Point otherLocation, double otherRadius) const
